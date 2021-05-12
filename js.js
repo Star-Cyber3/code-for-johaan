6a169a357d360dcc8bc6ad91d78258a44ba42bc2
Star-Cyber3 <80070589+Star-Cyber3@users.noreply.github.com>
2021-05-12 17:26:22 -0600

    sd

--- js.js
+++ js.js
@@ -1,4 +1,8 @@
-function johaan () {
-  const d = document.querySelector("#j");
-  d.innerHTML = "something else";
+const howTheTurnsOfTables = document.querySelector("#div");
+
+document.addEventListener('keydown', function1);
+
+
+function function1(evt) {
+  howTheTurnsOfTables.innerHTML += "evt"
 }
