e61a018f5ec310562756b8cb3ea32037645b1950
Star-Cyber3 <80070589+Star-Cyber3@users.noreply.github.com>
2021-06-16 16:49:16 -0600

    ok

--- js.js
+++ js.js
@@ -1,8 +1,21 @@
-const howTheTurnsOfTables = document.querySelector("#div");
+const howTheTurnsOfTables = document.querySelector("#something");
+let variable = 0;
 
 document.addEventListener('keydown', function1);
 
 
 function function1(evt) {
-  howTheTurnsOfTables.innerHTML += "evt"
+  for(var i = 0; i < 50; i++) {
+  if (variable == 0) {
+    howTheTurnsOfTables.innerHTML += "<span>" + "test" + "<span>";
+    variable = 1;
+  } else if (variable == 1) {
+    howTheTurnsOfTables.innerHTML += "test2 or 6969"
+    variable = 0; 
+  }
+}
+}
+
+function reset() {
+  howTheTurnsOfTables.innerHTML = "6969696969696969696969";
 }
